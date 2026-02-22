54221214980df17a4d94e0635134faa2aa410775
Zhukova Darya <71393466+DashZi@users.noreply.github.com>
2026-02-22 11:48:40 +0100

    minor stuff

--- exploration.sql
+++ exploration.sql
@@ -1,3 +1,15 @@
+USE OceanDB;
+GO
+
+SELECT COUNT(*) AS rows_count
+FROM dbo.realistic_ocean_climate_dataset;
+GO
+
+SELECT TOP 10 *
+FROM dbo.realistic_ocean_climate_dataset
+ORDER BY [Date];
+GO
+
 -- Средняя температура по локациям
 SELECT [Location], AVG(SST_C) AS avg_sst
 FROM dbo.realistic_ocean_climate_dataset
